d0f9796651300010c19ae767e1133a99b10528e8
“Krishna” <iamkrishv@gmail.com>
2019-02-22 09:00:01 +0100

    refactored and removed the duplicate lines in the unit testcase class

--- src/test/java/com/bowling/runner/BowlingGameTest.java
+++ src/test/java/com/bowling/runner/BowlingGameTest.java
@@ -4,23 +4,32 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+
 public class BowlingGameTest {
+	
+	private BowlingGame bowlingGame;
+
+	@Before
+	public void setUp() {
+		bowlingGame = new BowlingGame();
+	}
+
 	@Test
 	public void scoreGutterGame() {
-		BowlingGame bowlingGame = new BowlingGame();
-		bowlingGame.roll(0);
-		for (int i = 0; i < 20; i++) {
-			bowlingGame.roll(0);
-		}
+		roll(20, 0);
 		assertThat(bowlingGame.score(), is(0));
 	}
 
 	@Test
 	public void scoreGameOfOnes() {
-		BowlingGame bowlingGame = new BowlingGame();
-		for (int i = 0; i < 20; i++) {
-			bowlingGame.roll(1);
-		}
+		roll(20, 1);
 		assertThat(bowlingGame.score(), is(20));
+	}
+
+	public void roll(int times, int pinsDown) {
+		for (int i = 0; i < times; i++) {
+			bowlingGame.roll(pinsDown);
+		}
 	}
 }
