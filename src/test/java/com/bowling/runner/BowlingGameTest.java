f0977cacdc6807c425d1afc5a441b37817758e7a
“Krishna” <iamkrishv@gmail.com>
2019-02-22 08:45:26 +0100

    implemented unit testcase for gutter games also modified the dependency class as well

--- src/test/java/com/bowling/runner/BowlingGameTest.java
+++ src/test/java/com/bowling/runner/BowlingGameTest.java
@@ -1,11 +1,17 @@
 package com.bowling.runner;
 
 import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 
 public class BowlingGameTest {
 	@Test
 	public void rollBallAndZeroPinsDown() {
-		BowlingGame bowlingGame = new BowlingGame();
+		BowlingGame bowlingGame= new BowlingGame();
 		bowlingGame.roll(0);
+		for (int i = 0; i < 20; i++) {
+			bowlingGame.roll(0);
+		}
+		assertThat(bowlingGame.score(), is(0));
 	}
 }
