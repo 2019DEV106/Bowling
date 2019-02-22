06c7329057ed84ad331761025ddbb66fbf54b8b6
“Krishna” <iamkrishv@gmail.com>
2019-02-22 09:29:20 +0100

    refactored the conditional statement in the bowling class

--- src/main/java/com/bowling/runner/BowlingGame.java
+++ src/main/java/com/bowling/runner/BowlingGame.java
@@ -14,7 +14,7 @@ public class BowlingGame {
 		int cursor = 0;
 		for (int frame = 0; frame < 10; frame++) {
 
-			if (rolls[cursor] + rolls[cursor + 1] == 10) {
+			if (isSpare(cursor)) {
 				score += 10 + rolls[cursor + 2];
 				cursor += 2;
 			} else {
@@ -23,5 +23,9 @@ public class BowlingGame {
 			}
 		}
 		return score;
+	}
+	
+	public boolean isSpare(int cursor) {
+		return rolls[cursor] + rolls[cursor+1] ==10;
 	}
 }
